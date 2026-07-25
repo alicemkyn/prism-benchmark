94d5a1bd051cf0f4389ee3341247771e07e0f1be
Alicem Koyun <alicemkoyun@gmail.com>
2026-07-25 15:48:57 -0400

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prism-bench",
-    version="0.1.0",
+    version="0.2.0",
     description="PRISM: Pathology Reliability In Scarce-label Medicine benchmark",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
